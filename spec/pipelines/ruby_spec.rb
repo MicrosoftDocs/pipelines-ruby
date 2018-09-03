9203d198d7056835e64d62e803c24cb9a2e50c63
Prachi Bora <prachi.bora@microsoft.com>
2018-09-04 03:14:13 +0530

    Fix module method calls

--- spec/pipelines/ruby_spec.rb
+++ spec/pipelines/ruby_spec.rb
@@ -11,12 +11,12 @@ RSpec.describe Pipelines::Ruby do
   
   it "returns upper case for input_string" do
       expected_string = "YOUR LIFE IS THE SUM RESULT OF ALL THE CHOICES YOU MAKE."
-      expect(string_ops.to_upper(input_string)).to eq(expected_string)
+      expect(Pipelines::Ruby.to_upper(input_string)).to eq(expected_string)
   end
 
   it "returns lower case for input_string" do
       expected_string = "your life is the sum result of all the choices you make."
-      expect(string_ops.to_lower(input_string)).to eq(expected_string)
+      expect(Pipelines::Ruby.to_lower(input_string)).to eq(expected_string)
   end
 
 end
