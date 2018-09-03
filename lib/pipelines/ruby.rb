c3bf631d00adc78e4f417edac2244495d3c4546c
Prachi Bora <prachi.bora@microsoft.com>
2018-09-04 02:44:07 +0530

    Add string ops module

--- lib/pipelines/ruby.rb
+++ lib/pipelines/ruby.rb
@@ -4,4 +4,19 @@ module Pipelines
   module Ruby
     "Hello!"
   end
+  
+  module StringOps
+    def to_upper(string)
+      string.upcase
+    end
+
+    def to_lower(string)
+      string.downcase
+    end
+
+    def to_empty(string)
+      ""
+    end
+  end
+  
 end
