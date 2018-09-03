3b2c3f418a08936e330edcc9bba6872fa6714aeb
Prachi Bora <prachi.bora@microsoft.com>
2018-09-04 03:16:27 +0530

    Update ruby_spec.rb

--- spec/pipelines/ruby_spec.rb
+++ spec/pipelines/ruby_spec.rb
@@ -11,12 +11,12 @@ RSpec.describe Pipelines::Ruby do
   
   it "returns upper case for input_string" do
       expected_string = "YOUR LIFE IS THE SUM RESULT OF ALL THE CHOICES YOU MAKE."
-      expect(Pipelines::Ruby.to_upper(input_string)).to eq(expected_string)
+      expect(Pipelines::StringOps.to_upper(input_string)).to eq(expected_string)
   end
 
   it "returns lower case for input_string" do
       expected_string = "your life is the sum result of all the choices you make."
-      expect(Pipelines::Ruby.to_lower(input_string)).to eq(expected_string)
+      expect(Pipelines::StringOps.to_lower(input_string)).to eq(expected_string)
   end
 
 end
