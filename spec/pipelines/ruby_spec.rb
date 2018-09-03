07409183dbae4057376d82c747acc4425280888a
Prachi Bora <prachi.bora@microsoft.com>
2018-09-04 02:51:05 +0530

    Add a few more tests

--- spec/pipelines/ruby_spec.rb
+++ spec/pipelines/ruby_spec.rb
@@ -1,4 +1,6 @@
 RSpec.describe Pipelines::Ruby do
+  input_string = "Your life is the sum result of all the choices you make."
+  
   it "has a version number" do
     expect(Pipelines::Ruby::VERSION).not_to be nil
   end
@@ -6,4 +8,15 @@ RSpec.describe Pipelines::Ruby do
   it "does something useful" do
     expect(true).to eq(true)
   end
+  
+  it "returns upper case for input_string" do
+      expected_string = "YOUR LIFE IS THE SUM RESULT OF ALL THE CHOICES YOU MAKE."
+      expect(string_ops.to_upper(input_string)).to eq(expected_string)
+  end
+
+  it "returns lower case for input_string" do
+      expected_string = "your life is the sum result of all the choices you make."
+      expect(string_ops.to_lower(input_string)).to eq(expected_string)
+  end
+
 end
